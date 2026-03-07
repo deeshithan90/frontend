8641f8e805211dda6ee9073109b7fdb459333763
Your Name <deeshitn.tt@gmail.com>
2026-03-07 11:09:55 +0530

    new

--- src/AppRun.tsx
+++ src/AppRun.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
@@ -16,39 +16,36 @@ import Cookie from './pages/Cookie'
 import EnquiryPopup from './components/ContactUs'
 
 const AppRun = () => {
-  return (
-    <>
-    <Toaster position="top-right" reverseOrder={false} />
-      <Navbar/>
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/About' element={<About/>}/>
-          <Route path='/Inderste' element={<Industries/>}/>
-          <Route path='/products' element={<Products/>}/>
-          <Route path='/blogs' element={<Blogs/>}/>
-          <Route path='/productone' element={<Productone/>}/>
-          <Route path='/cookie' element={<Cookie/>}/>
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path='/:id/:id' element={<div className='flexs justify-center'>
-              <h1 className='text-red-500 mt-90 mb-30 text-5xl'>404 page not found</h1>
-          </div>}/>
-          <Route path='/:id' element={<div className='flexs justify-center'>
-              <h1 className='text-red-500 mt-90 mb-30 text-5xl'>404 page not found</h1>
-          </div>}/>
-          <Route path='/:id/:id/:id' element={<div className='flexs justify-center'>
-              <h1 className='text-red-500 mt-90 mb-30 text-5xl'>404 page not found</h1>
-          </div>}/>
-          <Route path='/:id/:id/:id/:id' element={<div className='flexs justify-center'>
-              <h1 className='text-red-500 mt-90 mb-30 text-5xl'>404 page not found</h1>
-          </div>}/>
-      </Routes>
-      <EnquiryPopup/>
-      <Footer/>
-      <Whatsapp/>
-    </>
-  )
+    return (
+        <>
+            <Toaster position="top-right" reverseOrder={false} />
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/inderste' element={<Industries />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/blogs' element={<Blogs />} />
+                <Route path='/productone' element={<Productone />} />
+                <Route path='/cookie' element={<Cookie />} />
+                <Route path="/admin" element={<AdminLogin />} />
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+                {/* 404 page */}
+                <Route path="*" element={
+                    <div className="flex justify-center">
+                        <h1 className="text-red-500 mt-90 mb-30 text-5xl">
+                            404 Page Not Found
+                        </h1>
+                    </div>
+                } />
+            </Routes>
+            <EnquiryPopup />
+            <Footer />
+            <Whatsapp />
+        </>
+    )
 }
 
 export default AppRun
